d2367095ec75783ffac668677914ff02c4b26686
Ivgeniay <motleycrue6502@gmail.com>
2026-04-06 00:34:31 +0300

    car.rs

---
+++ rust_core/core_lib/src/card/card.rs
@@ -0,0 +1,47 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Cost {
+    pub amount: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreatureData {
+    pub attack: u32,
+    pub health: u32,
+    pub max_health: u32
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WeaponData {
+    pub attack: u32,
+    pub durability: u32
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ArtifactData {
+    
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SpellData {
+
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum CardKind {
+    Creature(CreatureData),
+    Spell(SpellData),
+    Artifact(ArtifactData),
+    Weapon(WeaponData)
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Card {
+    pub id: Uuid,
+    pub expansion_id: Uuid,
+    pub name: String,
+    pub description: String,
+    pub cost: Cost,
+    pub kind: CardKind
+}
